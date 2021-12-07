7b94119e6c9685859542214fb53130b7235eb91b
Derrick Pua <derrickp@magenic.com>
2021-12-07 17:57:40 +0800

    Finish Homework 3

--- src/main/java/EquilateralTriangle.java
+++ src/main/java/EquilateralTriangle.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
 /**
  * TODO FOR HOMEWORK - EquilateralTriangle.java
- *      [ ] Update this class to inherit from BaseShape.java
- *      [ ] Update to a valid and logical constructor
- *      [ ] Override the area method
- *      [ ] Override the perimeter method
- *      [ ] Add a static main method
+ *      [X] Update this class to inherit from BaseShape.java
+ *      [X] Update to a valid and logical constructor
+ *      [X] Override the area method
+ *      [X] Override the perimeter method
+ *      [X] Add a static main method
  */
-public class EquilateralTriangle {
+public class EquilateralTriangle extends BaseShape {
+
     /**
      * TODO FOR HOMEWORK: Update the below constructor
      * NOTE: Remember you only need the length (Same thing as the base) to find the height of an equilateral triangle
@@ -14,12 +19,36 @@ public class EquilateralTriangle {
      * Creates an instance of the EquilateralTriangle class
      * @param length The base length of the triangle
      */
-    public EquilateralTriangle() {
+    public EquilateralTriangle(double length) {
+        super("Equilateral Triangle", length, length);
+    }
+
+    public double getHeight() {
+        return sqrt(3) * getLength() / 2;
     }
 
     // TODO FOR HOMEWORK: Override the area() method from BaseShape to work for a triangle
+    public double area() {
+        double height = sqrt(3) * getLength() / 2;
+        return (getLength() * height)/2;
+    }
 
     // TODO FOR HOMEWORK: Override the perimeter() method from BaseShape to work for a triangle
+    public double perimeter() {
+        return 3 * this.getLength();
+    }
 
     // TODO FOR HOMEWORK: Add a static main method for any debugging purposes
+    public static void main(String[] args) {
+        EquilateralTriangle t = new EquilateralTriangle(2);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter side length:");
+        int length = Integer.parseInt(scanner.nextLine());
+
+        t.setLength(length);
+
+        System.out.println("A triangle with side length of " + t.getLength() +  " has an area of: " + t.area());
+        System.out.println("A triangle with side length of " + t.getLength() + " has a perimeter of: " + t.perimeter());
+    }
 }
--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 /**
  * TODO FOR HOMEWORK - Rectangle.java
- *      [ ] Update this class to inherit from BaseShape.java
- *      [ ] Update to a valid and logical constructor
- *      [ ] Add a static main method
+ *      [X] Update this class to inherit from BaseShape.java
+ *      [X] Update to a valid and logical constructor
+ *      [X] Add a static main method
  */
-public class Rectangle {
+public class Rectangle extends BaseShape {
 
     /**
      * TODO FOR HOMEWORK: Update the below constructor
@@ -12,8 +14,23 @@ public class Rectangle {
      * @param length The length of the rectangle
      * @param height The width of the rectangle
      */
-    public Rectangle() {
+    public Rectangle(double length, double height) {
+        super("Rectangle", length, height);
     }
 
     // TODO FOR HOMEWORK: Add a static main method for any debugging purposes
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(2,4);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter side length:");
+        int length = Integer.parseInt(scanner.nextLine());
+        r.setLength(length);
+        System.out.println("Enter height:");
+        int height = Integer.parseInt(scanner.nextLine());
+        r.setHeight(height);
+
+        System.out.println("A rectangle with side length of " + r.getLength() + " and height of " +r.getHeight() + " has an area of: " + r.area());
+        System.out.println("A triangle with side length of " + r.getLength() + " and height of " +r.getHeight() + " has a perimeter of: " + r.perimeter());
+    }
 }
